0274eff9ee262c1eb30aaf2c8ab6224174a2edc2
Inna Suknovalnik <inna.pushistaya@gmail.com>
2017-12-12 10:39:54 +0700

    Рейтинг - фильтр  моб.

--- source/scripts/rating.js
+++ source/scripts/rating.js
@@ -2,6 +2,9 @@
   var currenciesLink = document.querySelector(".rating-filter__currencies-link");
   var currencies = document.querySelector(".rating-filter__currencies");
 
+  var filterLink = document.querySelector(".rating-filter__title");
+  var filterForm = document.querySelector(".rating-filter__form");
+  
   currenciesLink.addEventListener("click", function(event) {
     event.preventDefault();
     currenciesLink.classList.toggle("rating-filter__currencies-link_closed");
@@ -9,4 +12,13 @@
     currencies.classList.toggle("rating-filter__currencies_closed");
     currencies.classList.toggle("rating-filter__currencies_opened");
   });
+  
+  filterLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    filterLink.classList.toggle("rating-filter__title_closed");
+    filterLink.classList.toggle("rating-filter__title_opened");
+    filterForm.classList.toggle("rating-filter__form_closed");
+    filterForm.classList.toggle("rating-filter__form_opened");
+  });
+
 })();
